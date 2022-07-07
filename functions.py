b37e45aae37bb0194006a176a0cbb1bd726195bf
NartikoevNicholas <nartikoevnikolai@gmail.com>
2022-07-07 23:41:31 +0300

    task complate

---
+++ functions.py
@@ -0,0 +1,76 @@
+import math
+import requests
+from const import url_main
+from random import choice
+from bs4 import BeautifulSoup
+from urllib.parse import unquote
+
+
+# Функция, которая возвращает строку с единицами и нулями, где кол-е единиц равное "count_one", а нулей "count_zero"
+def get_array(count_one: int, count_zero: int) -> str:
+    return "1" * count_one + "0" * count_zero
+
+
+# Функция, которая принимает целое число и возвращает целое число деленное на 2
+def get_index(h: int) -> int:
+    return math.ceil(h / 2)
+
+
+# Функция, которая преобразует что-то в список строк с порядковым номером
+def get_list(data_list: list, count: int = 0) -> list:
+    result: list = []
+    if count != 0:
+        for i in range(count):
+            result.append(str(data_list[0] + i))
+        return result
+    else:
+        for i in range(int(len(data_list)/2)):
+            for j in range(data_list[(i * 2) + 1]-data_list[i*2]):
+                result.append(str(data_list[i*2] + j))
+        return result
+
+
+# Функция, которая получает html страницу
+def get_main_html(url, proxy_list: list, ua_list: list) -> requests:
+    while True:
+        proxy: str = choice(proxy_list)
+        ua: str = choice(ua_list)
+        proxies: dict = {'http': 'http://' + proxy}
+        headers: dict = {'User-Agent': ua}
+        result: requests = get_html(url, headers, proxies)
+        if result is not None and result.status_code == 200:
+            return result
+
+
+def get_data(url: str, character: str, proxy: list, user_agent: list) -> list:
+    result = []
+    while True:
+        html = get_main_html(url, proxy, user_agent)
+        soup = BeautifulSoup(html.text, 'lxml')
+        list_animals = soup.find_all("div", class_="mw-content-ltr")[-1].find("ul").find_all("li")
+        for animal in list_animals:
+            if animal.text[0] == character:
+                result.append(animal.text)
+            else:
+                return result
+        url = url_main + unquote(soup.find("a", text="Следующая страница")['href'])
+
+
+# вспомогательная функция
+def get_html(url, headers=None, proxies=None):
+    try:
+        response = requests.get(url, headers=headers, proxies=proxies)
+        if response.status_code == 200:
+            return response
+        else:
+            return None
+    except Exception as ex:
+        print(ex)
+
+
+# вспомогательная функция
+def get_ip(protocol: str, ip: str, port: str) -> dict:
+    result: dict = {}
+    if protocol == "http":
+        result["http"] = ip + ":" + port
+    return result
---
+++ main.py
@@ -0,0 +1,94 @@
+import time
+from update_proxy_useragent import update
+from multiprocessing import Pool
+from const import tests, alphabet_list, url_wikipedia
+from functions import *
+
+
+def task(array: str) -> int:
+    len_array: int = len(array)
+    h: int = get_index(len_array)
+    h_temp: int = 0
+    while True:
+        if array[h] == "1":
+            if array[h + 1] == "0":
+                return h + 1
+            h_temp = h
+            h += get_index(len_array - h)
+        else:
+            if array[h - 1] == "1":
+                return h
+            h -= get_index(h - h_temp)
+
+
+def handler(character):
+    proxy_list: list = open("proxy.txt").read().split("\n")
+    ua_list: list = open("ua.txt").read().split("\n")
+    result: list = get_data(url_wikipedia.replace("from=", f"from={character}"), character, proxy_list, ua_list)
+    return f"{character}: {len(result)}\n"
+
+
+def appearance(intervals):
+    result: int = 0
+    list_lesson = get_list(intervals['lesson'])
+    list_pupil = get_list(intervals['pupil'])
+    list_tutor = get_list(intervals['tutor'])
+
+    for _ in range(len(list_lesson)):
+        if list_pupil.__contains__(list_lesson[_]) and list_tutor.__contains__(list_lesson[_]):
+            result += 1
+    return result
+
+
+if __name__ == '__main__':
+    # Задача №1.
+    # Дан массив чисел, состоящий из некоторого количества подряд идущих единиц,
+    # за которыми следует какое-то количество подряд идущих нулей: 111111111111111111111111100000000
+    print("Задача №1")
+
+    # массив единиц и нулей -> string
+    array_string = get_array(2000000000, 1000000000)
+    # мое решение
+    start = time.time()
+    index = task(array_string)
+    end = time.time()
+    print(f"Мое решение: {index}. Время: {end - start}")
+
+    # решение python
+    start = time.time()
+    index = array_string.index("0")
+    end = time.time()
+    print(f"Решение python: {index}. Время: {end - start}")
+
+    # Задача №2.
+    # Получить с русской википедии список всех животных (https://inlnk.ru/jElywR) и вывести количество животных на
+    # каждую букву алфавита. Результат должен получиться в следующем виде:
+    # А: 642
+    # Б: 412
+    # В:....
+    print("Задача №2")
+    # proxy_list: list = open("proxy.txt").read().split("\n")
+    # ua_list: list = open("ua.txt").read().split("\n")
+    update()
+    with Pool(16) as pool:
+        result_list = pool.map(handler, alphabet_list)
+    print(result_list)
+
+    # Задача №3
+    # Мы сохраняем время присутствия каждого пользователя на уроке в виде интервалов. В функцию передается словарь,
+    # содержащий три списка с таймстемпами (время в секундах):
+    # lesson – начало и конец урока
+    # pupil – интервалы присутствия ученика
+    # tutor – интервалы присутствия учителя
+    # Интервалы устроены следующим образом – это всегда список из четного количества элементов. Под четными индексами
+    # (начиная с 0) время входа на урок, под нечетными - время выхода с урока.
+    # Нужно написать функцию, которая получает на вход словарь с интервалами и возвращает время общего присутствия
+    # ученика и учителя на уроке (в секундах).
+    print("Задача №3")
+
+    start = time.time()
+    for i, test in enumerate(tests):
+        test_answer = appearance(test['data'])
+        assert test_answer == test['answer'], f'Error on test case {i}, got {test_answer}, expected {test["answer"]}'
+    end = time.time()
+    print(end - start)
